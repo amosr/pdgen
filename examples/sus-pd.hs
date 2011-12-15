6db74151c2092bc21dfdbfd89e98eecd872f4ddb
Amos Robinson <amos.robinson@gmail.com>
2011-12-16 02:21:17 +1100

    initial commit untyped

---
+++ examples/sus-pd.hs
@@ -0,0 +1,51 @@
+import Language.Pd.PdGen
+import Language.Pd.PdGen.Out
+
+sustain :: Patch
+sustain = patch$ do
+	note <- object "inlet" ["note"]
+	vel <- object "inlet" ["vel"]
+	pedal <- object "inlet" ["pedal"]
+
+	swap <- object "swap" []
+
+	note@+0 `into` swap@-0
+	vel@+0 `into` swap@-1
+
+	pack <- object "pack" ["0", "0"]
+	swap@+0 `into` pack@-0
+	swap@+1 `into` pack@-1
+
+	route <- object "route" ["0"]
+	pack@+0 `into` route@-0
+
+	dam <- object "dam" []
+	route@+0 `into` dam@-0
+
+	eqz <- object "==" ["0"]
+	pedal@+0 `into` eqz@-0
+	eqz@+0 `into` dam@-1
+
+	tfb <- object "t" ["f", "b"]
+	dam@+0 `into` tfb@-0
+
+
+	outnote <- object "outlet" ["note"]
+	outvel <- object "outlet" ["vel"]
+
+	tfb@+0 `into` outnote@-0
+
+	mz <- message "0"
+	tfb@+1 `into` mz@-0
+	mz@+0 `into` outvel@-0
+
+	unpack <- object "unpack" []
+	route@+1 `into` unpack@-0
+
+	unpack@+0 `into` outnote@-0
+	unpack@+1 `into` outvel@-0
+
+	return ()
+
+main = do
+	putStrLn (out sustain)
---
+++ src/Language/Pd/PdGen.hs
@@ -0,0 +1,62 @@
+module Language.Pd.PdGen where
+
+import Control.Monad.State
+
+data Object
+	= Object String [String]
+	| Message String
+	| FloatAtom -- todo lower, upper bounds, send/rcv names
+	| SymbolAtom
+	-- text? array?
+	deriving (Eq,Show)
+newtype ObjectRef = ObjectRef Int
+	deriving (Eq,Show)
+	
+type Port = (ObjectRef,Int)
+newtype Inlet = Inlet Port
+	deriving (Eq,Show)
+newtype Outlet = Outlet Port
+	deriving (Eq,Show)
+
+data Patch = Patch {
+	objects :: [Object],
+	connections :: [(Outlet,Inlet)] }
+	deriving (Eq,Show)
+
+empty :: Patch
+empty = Patch [] []
+
+objectRefs :: Patch -> [ObjectRef]
+objectRefs p = map ObjectRef [0..length (objects p)]
+
+type Pd a = State Patch a
+
+patch :: Pd a -> Patch
+patch p = snd$ runState p empty
+
+insertObject :: Object -> Pd ObjectRef
+insertObject o = do
+	p@(Patch { objects = objs }) <- get
+	put (p{ objects = objs ++ [o] })
+	return$ ObjectRef (length objs)
+
+object :: String -> [String] -> Pd ObjectRef
+object name args = insertObject$ Object name args
+
+message :: String -> Pd ObjectRef
+message = insertObject . Message
+-- etc...
+
+infixl 9 @+
+(@+) :: ObjectRef -> Int -> Outlet
+(@+) o num = Outlet (o,num)
+
+infixl 9 @-
+(@-) :: ObjectRef -> Int -> Inlet
+(@-) o num = Inlet (o,num)
+
+infixl 5 `into`
+into :: Outlet -> Inlet -> Pd ()
+into o i = do
+	p@(Patch { connections = conns }) <- get
+	put (p{ connections = conns ++ [(o,i)] })
